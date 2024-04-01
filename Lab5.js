ff2ccb538fcd2b2b8df4aba228acccc85ff0c90b
Karan Joisher <karanjoisher@gmail.com>
2024-04-01 14:54:01 -0400

    3.2

--- Lab5.js
+++ Lab5.js
@@ -1,4 +1,42 @@
+const assignment = {
+  id: 1, title: "NodeJS Assignment",
+  description: "Create a NodeJS server with ExpressJS",
+  due: "2021-10-10", completed: false, score: 0,
+};
+
+const module = {
+  id: "mod1",
+  name: "Module 1",
+  description: "This is Module 1",
+  course: "CS5600"
+
+};
+
 const Lab5 = (app) => {
+  app.get("/a5/assignment", (req, res) => {
+    res.json(assignment);
+  });
+  app.get("/a5/module", (req, res) => {
+    res.json(module);
+  });
+  app.get("/a5/assignment/title", (req, res) => {
+    res.json(assignment.title);
+  });
+  app.get("/a5/module/name", (req, res) => {
+    res.json(module.name);
+  });
+  app.get("/a5/assignment/title/:newTitle", (req, res) => {
+    const { newTitle } = req.params;
+    assignment.title = newTitle;
+    res.json(assignment);
+  });
+  app.get("/a5/module/name/:newName", (req, res) => {
+    const { newName } = req.params;
+    module.name = newName;
+    res.json(module);
+  });
+
+
   app.get("/a5/welcome", (req, res) => {
     res.send("Welcome to Assignment 5");
   });
