d53ce0e2255c55a0319d69139b2c789dfa226aef
Karan Joisher <karanjoisher@gmail.com>
2024-04-01 14:25:29 -0400

    3.1

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import express from 'express'
-import Hello from "./Hello.js"
-const app = express()
-Hello(app)
-app.listen(4000)
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5.js";
+const app = express();
+app.use(express.json());
+Lab5(app);
+Hello(app);
+app.listen(4000);
