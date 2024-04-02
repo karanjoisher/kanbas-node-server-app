dff079a2cb1a1700359f393a9d3ae6f670f68333
Karan Joisher <karanjoisher@gmail.com>
2024-04-02 15:07:42 -0400

    4.3.5

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
-
+import AssignmentRoutes from './Kanbas/assignments/routes.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
+AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
