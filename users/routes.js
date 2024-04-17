afc6510870218daa39d7d5230986f6dfb32e291f
Karan Joisher <karanjoisher@gmail.com>
2024-04-17 17:36:16 -0400

    4.3

--- users/routes.js
+++ users/routes.js
@@ -5,16 +5,30 @@ export default function UserRoutes(app) {
     const user = await dao.createUser(req.body);
     res.json(user);
   };
-  
 
-  const deleteUser = async (req, res) => { };
+
+  const deleteUser = async (req, res) => {
+    console.log(req.params);
+    const status = await dao.deleteUser(req.params.userId);
+    res.json(status);
+  };
 
   const findAllUsers = async (req, res) => {
+    const { role } = req.query;
+    if (role) {
+      const users = await dao.findUsersByRole(role);
+      res.json(users);
+      return;
+    }
     const users = await dao.findAllUsers();
     res.json(users);
+    return;
   };
-  
-  const findUserById = async (req, res) => { };
+
+  const findUserById = async (req, res) => {
+    const user = await dao.findUserById(req.params.userId);
+    res.json(user);
+  };
   const updateUser = async (req, res) => {
     const { userId } = req.params;
     const status = await dao.updateUser(userId, req.body);
@@ -22,9 +36,16 @@ export default function UserRoutes(app) {
     res.json(status);
   };
 
+
+
+
   const signup = async (req, res) => {
-    const { username, password } = req.body;
-    currentUser = await dao.findUserByCredentials(username, password);
+    const user = await dao.findUserByUsername(req.body.username);
+    if (user) {
+      res.status(400).json(
+        { message: "Username already taken" });
+    }
+    currentUser = await dao.createUser(req.body);
     res.json(currentUser);
   };
   const signin = async (req, res) => {
@@ -32,7 +53,11 @@ export default function UserRoutes(app) {
     currentUser = await dao.findUserByCredentials(username, password);
     res.json(currentUser);
   };
-  const signout = (req, res) => { };
+  const signout = (req, res) => {
+    currentUser = null;
+    res.sendStatus(200);
+  };
+
   const profile = async (req, res) => {
     res.json(currentUser);
   };
@@ -45,4 +70,5 @@ export default function UserRoutes(app) {
   app.post("/api/users/signin", signin);
   app.post("/api/users/signout", signout);
   app.post("/api/users/profile", profile);
+  app.post("/api/users/signout", signout);
 }
